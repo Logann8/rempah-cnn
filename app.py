a14a062b324fc297ea881eb7ba30a581eea54908
Logann8 <rianhasra@gmail.com>
2025-02-04 20:09:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -65,5 +65,5 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Gunakan port dari Railway
+    port = int(os.environ.get("PORT", 8080))  # Gunakan port dari Railway
     app.run(host="0.0.0.0", port=port, debug=True)
